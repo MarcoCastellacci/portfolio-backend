4b86bdebb24c034a48500a5452e69a48bf09e0a2
Marco Castellacci <marco.castellacci87@gmail.com>
2022-07-30 16:34:02 -0300

    first commit

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,13 @@
+
+import Home from './Pages/Home.jsx';
+import './Styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
