11d73c54d524119e6eefaeb265f6919fa03be696
Marco Castellacci <marco.casterllacci87@gmail.com>
2022-08-01 18:21:31 -0300

    Adding Skills

--- my-portfolio/src/Pages/Home.jsx
+++ my-portfolio/src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Skills from '../Components/Skills';
 import '../Styles/styles.css'
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     return (
         <>
             <div className="home-container">
+            <Skills />
             </div>
         </>
     )
