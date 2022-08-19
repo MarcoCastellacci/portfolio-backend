8e6faede9b91a6baadacc0e8351d4d8876154e36
Marco Castellacci <marco.castellacci87@gmail.com>
2022-08-19 01:19:12 -0300

    contact Page and small changes

--- my-portfolio/src/Components/Footer.jsx
+++ my-portfolio/src/Components/Footer.jsx
@@ -68,7 +68,7 @@ export default function App() {
             </MDBContainer>
 
             <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-                © 2020 Copyright:
+                © 2022 Copyright:
                 <p className='text-white'>
                     Marco Castellacci
                 </p>
