32df199fb2c829e00e552f3e2fcb3f15088cd889
Marco Castellacci <marco.castellacci87@gmail.com>
2022-08-18 21:50:50 -0300

    Backen server and controllers

---
+++ models/proyect.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const ProyectSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    image: {type: String, required: true},
+    description: {type: String, required: true},
+    link: {type: String, required: true},
+    logo: {type: String, required: true},
+})
+
+const Proyect = mongoose.model('proyect', ProyectSchema)
+module.exports = Proyect
--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 require('dotenv').config()
 require('./config/database')
+
+const path = require('path')
+const cors = require('cors')
+const passport = require('passport')
 const express = require('express')
+const Router = require('./routes/routes')
 const app = express()
 
+
 const PORT = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+app.use(passport.initialize())
+app.use('/api', Router)
 
 app.get('/', (req, res) => {
     res.send('Server is running')
