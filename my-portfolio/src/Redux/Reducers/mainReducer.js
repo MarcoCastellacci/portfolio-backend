c1cd4839e430ebf0bc93dee4b2b9310352a467d9
Marco Castellacci <marco.casterllacci87@gmail.com>
2022-08-26 13:20:50 -0300

    Just Backend

--- my-portfolio/src/Redux/Reducers/mainReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import tecnoReducer from "../Reducers/tecnoReducers";
-import pagesReducer from "../Reducers/pagesReducer";
-
-const mainReducer = combineReducers({tecnoReducer, pagesReducer});
-
-export default mainReducer;
