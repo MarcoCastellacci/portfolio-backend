f4f51d033a82858c02902958351426a1b06656b7
Marco Castellacci <marco.castellacci87@gmail.com>
2022-11-05 00:04:45 -0300

    change firebase

--- config/database.js
+++ config/database.js
@@ -4,5 +4,7 @@ mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
+
+
 .then(() => console.log('Database is Connected'))
 .catch(err => console.error(err))
