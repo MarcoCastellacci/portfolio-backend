effb61eefafd91af5bb00c4580e8bd1be5096fea
Marco Castellacci <marco.castellacci87@gmail.com>
2022-11-04 22:32:06 -0300

    firebase login

--- models/proyect.js
+++ models/proyect.js
@@ -7,6 +7,5 @@ const ProyectSchema = new mongoose.Schema({
     link: {type: String, required: true},
     logo: {type: String, required: true},
 })
-
 const Proyect = mongoose.model('proyect', ProyectSchema)
 module.exports = Proyect
